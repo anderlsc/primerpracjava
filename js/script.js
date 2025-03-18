a062bbe5f24f4d0a68049c87759dcc6f28c9873b
anderlsc <andersonsobalvarro2006@gmail.com>
2025-03-18 16:12:52 -0600

    hola

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",function()
+{
+document.getElementById("miboton").addEventListener("click",function()
+    {
+     saludar();
+     saludar2();
+    }
+);
+
+});
+
+function saludar()
+{
+    alert ("hola mundo");
+    alert ("hola mundo2");
+}
