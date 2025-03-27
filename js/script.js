29d357be1c3c867a983316c62c48b3f2cc0a6d04
anderlsc <andersonsobalvarro2006@gmail.com>
2025-03-27 16:45:10 -0600

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded",function()
 {
 document.getElementById("mi_boton").addEventListener("click", function()
@@ -10,16 +12,17 @@ document.getElementById("btn_color").addEventListener("click", function()
 cambiar_color();
 });
 
-function mostrar_mensaje() {
+function mostrar_mensaje() 
+{
     //Declaracion de variable
     var datouno;
     var datodos;
     //Asignacion de id a las variables
 
-    datouno = document.getElementById("imput1").value;
-    datodos = document.getElementById("imput2").value;
+    datouno = document.getElementById("input1").value;
+    datodos = document.getElementById("input2").value;
 
-    var mensaje = "Los datos son " + datouno + " " + datodos;
+    var mensaje = "Su Nombre Es " + datouno + " " + datodos;
      //Llamar mensaje
 
     
@@ -31,27 +34,33 @@ function mostrar_mensaje() {
         document.getElementById("resultado").innerText = mensaje;
     }
 }
-function cambiar_color(){
+function cambiar_color()
+{
     var elemento = document.getElementById("contenedor");    
    
 
 
-    if (document.body.style.backgroundColor === "black"){
+    if (document.body.style.backgroundColor === "black")
+        {
         document.body.style.backgroundColor = "white";
         elemento.style.position="absolute";
-        elemento.style.width="200px";
-        elemento.style.height="200px";
+        elemento.style.width="150px";
+        elemento.style.height="150px";
         elemento.style.backgroundColor="blue";
     }
         
-    else{
+    else
+    {
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white"
         elemento.style.position="absolute";
         elemento.style.width="150px";
         elemento.style.height="150px";
         elemento.style.backgroundColor="red";
+     
     }
         
     
 }
+
+
